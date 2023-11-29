59d70dff90ec79d42a6bf52f9451bf4d53a6e66d
CompaoreYannDjamel <102490687+CompaoreYannDjamel@users.noreply.github.com>
2023-11-29 18:10:43 -0500

    Ajoutez des fichiers projet.

---
+++ main/Program.cs
@@ -0,0 +1,15 @@
+﻿using logique;
+using Logique;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.LancerJeu();
+            
+        }
+    }
+}
